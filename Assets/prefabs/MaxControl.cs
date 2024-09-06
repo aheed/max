8306080ddf4d9a1be3d09a3eaed43204ed25376b
Anders Heed <aheed88888@gmail.com>
2024-09-06 19:39:59 +0200

    add arrow key input

---
+++ Assets/prefabs/MaxControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MaxControl : MonoBehaviour
+{
+    public float playerSpeed = 3.0f;
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+	    rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * playerSpeed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
