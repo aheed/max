1ab6fe38d318c4b15255f47045298023ccd7c173
Anders Heed <aheed88888@gmail.com>
2024-09-18 11:04:15 +0200

    wip

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -8,14 +8,80 @@ public class SceneController : MonoBehaviour
     public EnemyPlane enemyPlanePrefab;
     public ShadowControl shadowControlPrefab;
     public refobj refobject;
+    public float width = 1;
+    public float height = 1;
 
     void AddPlaneShadow(Transform parent)
     {        
         Instantiate(shadowControlPrefab, transform.position, Quaternion.identity, parent);
     }
 
+    void CreateMesh(Vector3[] vertices)
+    {
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        Mesh mesh = new Mesh();
+        
+        mesh.vertices = vertices;
+
+        int[] tris = new int[6]
+        {
+            // lower left triangle
+            0, 2, 1,
+            // upper right triangle
+            2, 3, 1
+        };
+        mesh.triangles = tris;
+
+        Vector3[] normals = new Vector3[4]
+        {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward
+        };
+        mesh.normals = normals;
+
+        Vector2[] uv = new Vector2[4]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+        mesh.uv = uv;
+
+        meshFilter.mesh = mesh;
+    }
+
+    public void CreateBackground()
+    {
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+        CreateMesh(new Vector3[4]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(width, 0, 0),
+            new Vector3(0, height, 0),
+            new Vector3(width, height, 0)
+        });
+
+        float xskew = 0.2f;
+        float yoffset = 1.2f;
+        CreateMesh(new Vector3[4]
+        {
+            new Vector3(0, 0 + yoffset, 0),
+            new Vector3(width, 0 + yoffset, 0),
+            new Vector3(0 + xskew, height +  yoffset, 0),
+            new Vector3(width + xskew, height + yoffset, 0)
+        });
+    }
+
     void Start()
     {
+        CreateBackground();
+
         //var refobject = GetComponent<refobj>();
         var startPos = refobject.transform.position;
         /*startPos.x += 1.0f;
