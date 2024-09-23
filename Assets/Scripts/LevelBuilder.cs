7350eab1a357f91181a247feca4b5fb5fd14f1b0
Anders Heed <aheed88888@gmail.com>
2024-09-23 22:14:22 +0200

    implement LevelBuilder wip

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum CellContent
@@ -14,6 +17,12 @@ public enum CellContent
     TREE2
 }
 
+public class RiverSegment
+{
+    public int Height;
+    public int slopeIndex;
+}
+
 public class LevelBuilder 
 {
     public readonly int gridHeight = 300;
@@ -23,6 +32,13 @@ public class LevelBuilder
     public readonly int minSpaceBetweenRoads = 10;
     public readonly float roadProbability = 0.1f;
     public readonly int roadHeight = 2;
+    static readonly int[] riverSlopes = new int[] {-1, -1, 0, 1, 1};
+    static readonly int neutralRiverSlopeIndex = 2;
+    public int minDistanceRiverAirstrip = 10;
+    public int riverWidth = 6;
+    public int maxRiverSegmentHeight = 7;
+    public int minRiverSegmentHeight = 2;
+    public float approachQuotient = 0.2f;
     
     // Builds a 2D level including landing strip at beginning.
     // Never mind viewing perspective or screen position.
@@ -43,10 +59,12 @@ public class LevelBuilder
 
         // Roads        
         var cooldown = 0;
+        List<int> roads = new List<int>();
         for (var y = landingStripHeight + cooldown; y < (gridHeight - cooldown); y++)
         {
-            if (cooldown <= 0 && Random.Range(0f, 1.0f) < roadProbability)
+            if (cooldown <= 0 && UnityEngine.Random.Range(0f, 1.0f) < roadProbability)
             {
+                roads.Add(y);
                 for (var x = 0; x <= gridWidth; x++)
                 {
                     for (var i = 0; y < (y + roadHeight); y++, i++)
@@ -59,8 +77,71 @@ public class LevelBuilder
             cooldown--;
         }
         
-
         // River
+        var directionMultiplier = riverLeftOfAirstrip ? -1 : 1;
+        int riverLowerLeftCornerXStart = midX + directionMultiplier * minDistanceRiverAirstrip - (riverWidth / 2);
+        int riverLowerLeftCornerX = riverLowerLeftCornerXStart;
+        List<RiverSegment> riverSegments = new List<RiverSegment>();
+        for (var y = 0; y < gridHeight;)
+        {
+            var segmentHeight = UnityEngine.Random.Range(minRiverSegmentHeight, maxRiverSegmentHeight);
+            var maxSegmentY = gridHeight - y;
+            if (segmentHeight > maxSegmentY)
+            {
+                segmentHeight = maxSegmentY;
+            }
+
+            var midRiverX = riverLowerLeftCornerX + (riverWidth / 2);
+            riverLeftOfAirstrip = midRiverX < midX;
+            var minSlopeIndex = 1;
+            var maxSlopeIndexExclusive = riverSlopes.Length - 1;
+            var approachLength = (int)(gridHeight * approachQuotient);
+            bool approaching = gridHeight - y < approachLength;
+            bool takingOff = y < approachLength;
+            int slopeIndexOffset = 0;
+            if (approaching)
+            {
+                // Airstrip approaching. River must not bend toward next airstrip location.
+                slopeIndexOffset =  riverLeftOfAirstrip ? -1 : 1;
+            }
+            if (takingOff)
+            {
+                // Leaving Airstrip. River must not bend away from next airstrip location.
+                slopeIndexOffset =  riverLeftOfAirstrip ? 1 : -1;
+            }
+            minSlopeIndex += slopeIndexOffset;
+            maxSlopeIndexExclusive += slopeIndexOffset;
+            var slopeIndex = UnityEngine.Random.Range(minSlopeIndex, maxSlopeIndexExclusive);
+            riverSegments.Add(new RiverSegment {Height = segmentHeight, slopeIndex = slopeIndex});
+
+            var slopeX = riverSlopes[slopeIndex] * segmentHeight;
+
+            Debug.Log($"riverLowerLeftCornerX riverWidth slopeX y segmentHeight: {riverLowerLeftCornerX} {riverWidth} {slopeX} {y} {segmentHeight} {approaching} {takingOff} {minSlopeIndex} {maxSlopeIndexExclusive} {riverLeftOfAirstrip}");
+            
+            y += segmentHeight;
+            riverLowerLeftCornerX += slopeX;
+        }
+
+        var ytmp = 0;
+        var startX = riverLowerLeftCornerXStart;
+        foreach (var segment in riverSegments)
+        {
+            var newY = ytmp + segment.Height;            
+            for (var y = ytmp; y < newY; y++)
+            {
+                startX += riverSlopes[segment.slopeIndex];
+                for (var x = startX; x < riverWidth; x++)
+                {
+                    if (x >= 0 && x < gridWidth)
+                    {
+                        ret[x, y] = CellContent.WATER;
+                    }
+                }
+            }
+
+            ytmp = newY;
+        }
+
         // Houses
         // Tanks
         // Flack guns
