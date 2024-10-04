cd344a009c37ce80a65b3bd30c80af622585969e
Anders Heed <aheed88888@gmail.com>
2024-10-04 20:15:26 +0200

    limit bomb splash life span

---
+++ Assets/BombSplash.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSplash : MonoBehaviour
+{
+    public float lifeSpanSec = 1.2f;
+    float timeToLiveSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToLiveSec = lifeSpanSec;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToLiveSec -= Time.deltaTime;
+        if (timeToLiveSec < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
