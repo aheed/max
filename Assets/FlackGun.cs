f11c328557511d63d7a3c19f992deb43fbde2b9e
Anders Heed <aheed88888@gmail.com>
2024-10-05 18:38:51 +0200

    add flack gun vs bullet effect

--- Assets/FlackGun.cs
+++ Assets/FlackGun.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlackGun : MonoBehaviour
+public class FlackGun : MonoBehaviour, IPositionObservable
 {
-    public GameObject flackProjectilePrefab;    
+    public GameObject flackProjectilePrefab;
+    public Sprite shotSprite;
     public float avgTimeToShootSeconds = 5.0f;
     float timeToShoot = -1.0f;
     GameState gameState;
+    private SpriteRenderer spriteR;
 
     void RestartShotClock()
     {
@@ -19,23 +21,41 @@ public class FlackGun : MonoBehaviour
         Instantiate(flackProjectilePrefab, transform.position, Quaternion.identity);
     }
 
+    void HandleCollision(Collider2D col)
+    {
+        var collObjName = CollisionHelper.GetObjectWithOverlappingAltitude(this, col.gameObject);
+        if (collObjName.StartsWith("bullet"))
+        {
+            spriteR.sprite = shotSprite;
+            var collider = gameObject.GetComponent<Collider2D>();
+            if (collider != null)
+            {
+                collider.enabled = false;
+            }
+
+            // Todo: report destroyed flack gun for scoring
+        }
+
+        //no collision
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.name.StartsWith("bomb"))
         {
             var bomb = col.gameObject.GetComponent<Bomb>();
             gameState.BombLanded(bomb, gameObject);
+            return;
         }
-        else if (col.name.StartsWith("bullet"))
-        {
-            //todo
-        }
+
+        HandleCollision(col);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        RestartShotClock();
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        RestartShotClock();        
     }
 
     // Update is called once per frame
@@ -53,4 +73,8 @@ public class FlackGun : MonoBehaviour
             RestartShotClock();
         }
     }
+
+    public Vector2 GetPosition() => transform.position;
+    public float GetAltitude() => 0.1f;
+    public float GetHeight() => 0.41f;
 }
