74f42b1b9803ee766b45b28a59a6ee477005811e
Anders Heed <aheed88888@gmail.com>
2024-12-09 19:01:06 +0100

    add functional tv, fullscreen and mute buttons

--- Assets/ButtonBarDocument.cs
+++ Assets/ButtonBarDocument.cs
@@ -7,9 +7,16 @@ public class ButtonBarDocument : MonoBehaviour
 {
     public Texture2D crtTexture;
     public Texture2D flatscreenTexture;
+    public Texture2D fullScreenTexture;
+    public Texture2D exitFullScreenTexture;
+    public Texture2D volumeMuteTexture;
+    public Texture2D volumeUpTexture;
     VisualElement tvElem;
     VisualElement fullScreenElem;
+    VisualElement muteElem;
     GameState gameState;
+    float audioVolume;
+    bool fullScreen = false; //expected state, could change any time
     
 
     // Start is called before the first frame update
@@ -22,18 +29,50 @@ public class ButtonBarDocument : MonoBehaviour
         tvElem.RegisterCallback<ClickEvent>(OnTVClicked);
 
         fullScreenElem = uiDocument.rootVisualElement.Q<VisualElement>("FullScreenButton");
-        fullScreenElem.RegisterCallback<ClickEvent>(OnFullScreenClicked);
+        fullScreenElem.RegisterCallback<PointerDownEvent>(OnFullScreenClicked);
+
+        muteElem = uiDocument.rootVisualElement.Q<VisualElement>("MuteButton");
+        muteElem.RegisterCallback<ClickEvent>(OnMuteClicked);
+        UpdateAll();
     }
 
     void UpdateAll()
     {
         UpdateTvSimButton();
+        UpdateFullScreenButton();
+        UpdateMuteButton();
     }
 
     void UpdateTvSimButton()
     {
         var newTexture = gameState.viewMode == ViewMode.NORMAL ? crtTexture : flatscreenTexture;
         tvElem.style.backgroundImage = new StyleBackground(newTexture);
+    }
+
+    void UpdateFullScreenButton()
+    {        
+        var newTexture = Screen.fullScreen ? exitFullScreenTexture : fullScreenTexture;
+        fullScreenElem.style.backgroundImage = new StyleBackground(newTexture);
+    }
+
+    void CheckFullScreenButton()
+    {        
+        if (fullScreen != Screen.fullScreen)
+        {
+            UpdateFullScreenButton();
+            fullScreen = Screen.fullScreen;
+        }
+    }
+
+    void UpdateMuteButton()
+    {
+        var newTexture = AudioListener.volume == 0 ? volumeMuteTexture : volumeUpTexture;
+        muteElem.style.backgroundImage = new StyleBackground(newTexture);        
+    }
+
+    void Update()
+    {
+        CheckFullScreenButton();
     }
 
     void OnTVClicked(ClickEvent evt)
@@ -46,7 +85,7 @@ public class ButtonBarDocument : MonoBehaviour
         UpdateTvSimButton();
     }
 
-    void OnFullScreenClicked(ClickEvent evt)
+    void OnFullScreenClicked(PointerDownEvent evt)
     {
         Debug.Log("Fullscreen toggle clicked");
         // Only perform this action at the target, not in a parent
@@ -54,7 +93,27 @@ public class ButtonBarDocument : MonoBehaviour
         return;
 
         Screen.fullScreen = !Screen.fullScreen;
+        //UpdateFullScreenButton();
     }
 
-    
+    void OnMuteClicked(ClickEvent evt)
+    {
+        Debug.Log("Mute clicked");
+        if (evt.propagationPhase != PropagationPhase.AtTarget)
+        return;
+        
+        var audioListener = FindObjectOfType<AudioListener>(true); //assume there is only one
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = audioVolume;
+        }
+        else
+        {
+            audioVolume = AudioListener.volume;
+            AudioListener.volume = 0;
+        }
+        
+        //AudioListener.pause = !AudioListener.pause;
+        UpdateMuteButton();
+    }
 }
