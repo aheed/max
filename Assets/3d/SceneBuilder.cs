1c971d9e883ec586209c1ac6c25564e29eaf3ccd
Anders Heed <aheed88888@gmail.com>
2025-01-31 22:07:24 +0100

    hangar and enemy landing strip

--- Assets/3d/SceneBuilder.cs
+++ Assets/3d/SceneBuilder.cs
@@ -150,7 +150,6 @@ public class SceneBuilder : MonoBehaviour
             var meshSize =  lsMeshFilter.mesh.bounds.size;
             var localScale = lsGameObject.transform.localScale;
             localScale.x = lsWidth / (meshSize.x * lsQuadTransform.localScale.x);
-            //localScale.z = lsHeight / (meshSize.y * lsQuadTransform.localScale.y); // mesh y corresponds to world z because of the mesh orientation
             localScale.z = lsHeight / (meshSize.z * lsQuadTransform.localScale.z);
             lsGameObject.transform.localScale = localScale;
 
