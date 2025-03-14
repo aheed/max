28841fc6412c111967afff5037cbebc805eee8e2
Anders Heed <aheed88888@gmail.com>
2025-03-14 21:45:11 +0100

    make road vehicles shootable and bombable

--- Assets/3d/Vehicle3d.cs
+++ Assets/3d/Vehicle3d.cs
@@ -1,6 +1,24 @@
+using System.Globalization;
 using UnityEngine;
 
 public class Vehicle3d : ManagedObject
 {
-    
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject bombGameObject = null;
+        if (col.name.StartsWith("bullet", true, CultureInfo.InvariantCulture))
+        {
+            
+        }
+        else if (col.name.StartsWith("bomb", true, CultureInfo.InvariantCulture))
+        {
+            bombGameObject = col.gameObject;
+        }
+        else
+        {
+            return;
+        }
+
+        GameState.GetInstance().BombLanded(bombGameObject, gameObject);
+    }
 }
