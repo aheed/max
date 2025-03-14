d1f81e2eb6a795a5f606b71ec12b9f716125124a
Anders Heed <aheed88888@gmail.com>
2025-03-14 21:33:08 +0100

    make hangars bombable

--- Assets/Hangar3d.cs
+++ Assets/Hangar3d.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class Hangar3d : ManagedObject
 {
+    private bool alive = true;
 
+    void SetAlive(bool isAlive)
+    {
+        alive = isAlive;
+        transform.GetChild(0).gameObject.SetActive(isAlive);
+        transform.GetChild(1).gameObject.SetActive(!isAlive);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {        
+        Debug.Log($"Hangar Hit!!!!!!!!!!!!!!!  collided with {col.gameObject.name}");
+        
+        if (!col.gameObject.name.StartsWith("Bomb"))
+        {
+            return;
+        }
+
+        GameState.GetInstance().BombLanded(col.gameObject, new GameObject());
+        SetAlive(false);
+    }
+
+    public override void Reactivate()
+    {
+        if (!alive)
+        {
+            SetAlive(true);
+        }
+    }
 }
