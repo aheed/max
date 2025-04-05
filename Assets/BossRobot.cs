ff2d47adcb7d9898853c85018cffebf415c3f0dc
Anders Heed <aheed88888@gmail.com>
2025-04-05 20:41:58 +0200

    add approach stage

--- Assets/BossRobot.cs
+++ Assets/BossRobot.cs
@@ -2,14 +2,28 @@ using UnityEngine;
 
 public class BossRobot : MonoBehaviour
 {
+    enum BossRobotStage
+    {
+        APPROACHING,
+        FIGHTING,
+        DEFEATED
+    }
+
     public BossMissile missilePrefab;
     public GameObject targetObject;
     public float launchIntervalSec = 2.0f;
     public float missileStartOffsetZ = 0.3f;
+    public float fightDistance = 2.0f;
+    public float approachDistance = 8.0f;
+    public float approachSpeed = 1f;
 
+    GameObject refObject;
+    BossRobotStage stage = BossRobotStage.APPROACHING;
     float launchCooldown = 0.0f;
     BossMissile[] standbyMissiles = new BossMissile[2];
     int nextMissileIndex = 0;
+
+    
 
     Transform GetLauncherTransform(int missileIndex)
     {
@@ -49,12 +63,34 @@ public class BossRobot : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        refObject = transform.parent.gameObject;
+        var tmpPosition = refObject.transform.position;
+        tmpPosition.z += approachDistance;
+        transform.position = tmpPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (stage == BossRobotStage.APPROACHING)
+        {
+            if (refObject != null)
+            {
+                float distance = Vector3.Distance(transform.position, refObject.transform.position);
+                
+                // Move towards the target
+                Vector3 direction = (refObject.transform.position - transform.position).normalized;
+                transform.position += direction * approachSpeed * Time.deltaTime;
+
+                if (distance < fightDistance)
+                {
+                    stage = BossRobotStage.FIGHTING;
+                    Debug.Log("Boss Robot is now fighting!");
+                }                
+            }
+            return;
+        }
+
         launchCooldown -= Time.deltaTime;
         if (launchCooldown <= 0.0f)
         {
