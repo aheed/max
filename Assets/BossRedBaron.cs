0687ea2eb83779ca80be48705dc04429ff4fa129
Anders Heed <aheed88888@gmail.com>
2025-04-17 10:06:17 +0200

    make red baron move sideways

--- Assets/BossRedBaron.cs
+++ Assets/BossRedBaron.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class BossRedBaron : MonoBehaviour
 {
+    public float moveDelayMaxSec = 4.5f;
+    public float moveDelayMinSec = 1.5f;
+    public float offsetMaxX = 2.5f;
+
+    float moveCooldown = 0.0f;
+    Vector3 targetLocalPosition;
+    Vector3 startLocalPosition;
+
+    void ResetMoveCooldown()
+    {
+        //destinationOffset = GetRandomOffset();
+        targetLocalPosition = startLocalPosition + new Vector3(
+            Random.Range(-offsetMaxX, offsetMaxX),
+            0f, // TEMP
+            0f  // TEMP
+        );
+        moveCooldown = Random.Range(moveDelayMinSec, moveDelayMaxSec);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startLocalPosition = transform.localPosition;
+        ResetMoveCooldown();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveCooldown -= Time.deltaTime;
+        if (moveCooldown <= 0.0f)
+        {
+            ResetMoveCooldown();
+            
+        }
+
+        transform.localPosition = Vector3.MoveTowards(
+                transform.localPosition,
+                targetLocalPosition,
+                Time.deltaTime * 2.0f
+            );
     }
 }
