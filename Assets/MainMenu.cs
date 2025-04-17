9b7f9ddd45b3c6907aa72b5274eb256bec9920a3
Anders Heed <aheed88888@gmail.com>
2025-04-17 12:04:31 +0200

    add red baron level button to main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,6 +9,7 @@ public class MainMenu : MonoBehaviour
     Button twoDBalloonbutton;
     Button threeDbutton;
     Button threeDRobotButton;
+    Button threeDRedBaronButton;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,10 +19,12 @@ public class MainMenu : MonoBehaviour
         twoDBalloonbutton = uiDocument.rootVisualElement.Q<Button>("2dBalloon");
         threeDbutton = uiDocument.rootVisualElement.Q<Button>("3d");
         threeDRobotButton = uiDocument.rootVisualElement.Q<Button>("3dRobot");
+        threeDRedBaronButton = uiDocument.rootVisualElement.Q<Button>("3dRedBaron");
         twoDbutton.RegisterCallback<ClickEvent>(On2dClicked);
         twoDBalloonbutton.RegisterCallback<ClickEvent>(On2dBalloonClicked);
         threeDbutton.RegisterCallback<ClickEvent>(On3dClicked);
         threeDRobotButton.RegisterCallback<ClickEvent>(On3dRobotClicked);
+        threeDRedBaronButton.RegisterCallback<ClickEvent>(On3dRedBaronClicked);
         SceneManager.activeSceneChanged += ChangedActiveScene;
     }
 
