c5b34563f238e517853c1b6216f5d727bd6f8f46
Anders Heed <aheed88888@gmail.com>
2025-05-06 12:34:28 +0200

    swipe animation wip

--- Assets/ControlDocument.cs
+++ Assets/ControlDocument.cs
@@ -3,7 +3,10 @@ using UnityEngine.UIElements;
 
 public class ControlDocument : MonoBehaviour
 {
+    static readonly string swipeDownAnimationClass = "swipe-hint-down-animation";
+    static readonly int delayRerunAnimationMs = 600;
     VisualElement fireElem;
+    VisualElement downSwipeHintElem;
     //GameState gameState;
     GameStateContents gameStateContents;
 
@@ -16,7 +19,22 @@ public class ControlDocument : MonoBehaviour
         fireElem = uiDocument.rootVisualElement.Q<VisualElement>("FireButton");
         fireElem.RegisterCallback<PointerDownEvent>(OnFireDown);
         fireElem.RegisterCallback<PointerUpEvent>(OnFireUp);
-        fireElem.RegisterCallback<PointerOutEvent>(OnFireOut);        
+        fireElem.RegisterCallback<PointerOutEvent>(OnFireOut);
+
+        downSwipeHintElem = uiDocument.rootVisualElement.Q<VisualElement>("DownSwipeHint");
+        SetupSwipeAnimation();
+    }
+
+    void SetupSwipeAnimation()
+    {
+        downSwipeHintElem.RemoveFromClassList(swipeDownAnimationClass);
+        downSwipeHintElem.RegisterCallback<TransitionEndEvent>(evt =>
+        {
+            //downSwipeHintElem.RemoveFromClassList(swipeDownAnimationClass);
+            downSwipeHintElem.schedule.Execute(() => downSwipeHintElem.RemoveFromClassList(swipeDownAnimationClass)).StartingIn(delayRerunAnimationMs);
+            downSwipeHintElem.schedule.Execute(() => downSwipeHintElem.AddToClassList(swipeDownAnimationClass)).StartingIn(delayRerunAnimationMs + 10);
+        });
+        downSwipeHintElem.schedule.Execute(() => downSwipeHintElem.AddToClassList(swipeDownAnimationClass)).StartingIn(100);
     }
 
     // Update is called once per frame
