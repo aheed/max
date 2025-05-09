f73a3d75da8e48bba0fa06dbbeed6f3e05b27763
Anders Heed <aheed88888@gmail.com>
2025-05-09 18:00:57 +0200

    add main menu button for intro level

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
     UIDocument uiDocument;
     Button twoDbutton;
     Button twoDBalloonbutton;
+    Button threeDIntrobutton;
     Button threeDbutton;
     Button threeDRobotButton;
     Button threeDRedBaronButton;
@@ -18,10 +19,12 @@ public class MainMenu : MonoBehaviour
         twoDbutton = uiDocument.rootVisualElement.Q<Button>("2d");
         twoDBalloonbutton = uiDocument.rootVisualElement.Q<Button>("2dBalloon");
         threeDbutton = uiDocument.rootVisualElement.Q<Button>("3d");
+        threeDIntrobutton = uiDocument.rootVisualElement.Q<Button>("3dIntro");
         threeDRobotButton = uiDocument.rootVisualElement.Q<Button>("3dRobot");
         threeDRedBaronButton = uiDocument.rootVisualElement.Q<Button>("3dRedBaron");
         twoDbutton.RegisterCallback<ClickEvent>(On2dClicked);
         twoDBalloonbutton.RegisterCallback<ClickEvent>(On2dBalloonClicked);
+        threeDIntrobutton.RegisterCallback<ClickEvent>(On3dIntroClicked);
         threeDbutton.RegisterCallback<ClickEvent>(On3dClicked);
         threeDRobotButton.RegisterCallback<ClickEvent>(On3dRobotClicked);
         threeDRedBaronButton.RegisterCallback<ClickEvent>(On3dRedBaronClicked);
@@ -45,6 +48,14 @@ public class MainMenu : MonoBehaviour
         
         // Load 2D scene, balloons level
         SceneManager.LoadScene("2DScene");
+    }
+
+    void On3dIntroClicked(ClickEvent evt)
+    {
+        Debug.Log("3D Intro button clicked");
+        LevelSelection.startLevelOverride = true;
+        LevelSelection.startLevel = LevelType.INTRO;
+        SceneManager.LoadScene("3DScene");
     }
 
     void On3dClicked(ClickEvent evt)
