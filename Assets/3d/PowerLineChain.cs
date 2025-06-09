07706578d45b1fe5c75fbc50b406cbb838b3b4d5
Anders Heed <aheed88888@gmail.com>
2025-06-09 11:34:12 +0200

    reuse identical power line objects

--- Assets/3d/PowerLineChain.cs
+++ Assets/3d/PowerLineChain.cs
@@ -9,35 +9,46 @@ public class PowerLineChain : ManagedObject
     public float powerLineDistanceZ = 0.1f;
     public float powerLineAltitude = 2.5f;
     static float levelWidth;
+    static GameObject InnerTemplate;
+    GameObject Inner;
 
     public static void SetLevelWidth(float width)
     {
         levelWidth = width;
+        InnerTemplate = null;
     }
 
     void Start()
     {
-        var lineOffsetX = (powerLineSegmentLength / 2) - (levelWidth / 2);
-        var postOffsetX = -(levelWidth / 2);
-
-        for (float x = 0; x < levelWidth; x += powerLineSegmentLength)
+        if (InnerTemplate == null)
         {
-            var powerX = x + lineOffsetX;
+            InnerTemplate = new GameObject("PowerLineChainInner");
 
-            var powerLineGameObject1 = Instantiate(
-                powerLinePrefab,
-                transform);
-            powerLineGameObject1.transform.localPosition = new Vector3(powerX, powerLineAltitude, -powerLineDistanceZ);
+            var lineOffsetX = (powerLineSegmentLength / 2) - (levelWidth / 2);
+            var postOffsetX = -(levelWidth / 2);
 
-            var powerLineGameObject2 = Instantiate(
-                powerLinePrefab,
-                transform);
-            powerLineGameObject2.transform.localPosition = new Vector3(powerX, powerLineAltitude, powerLineDistanceZ);
+            for (float x = 0; x < levelWidth; x += powerLineSegmentLength)
+            {
+                var powerX = x + lineOffsetX;
 
-            var powerPostGameObject = Instantiate(
-                powerPostPrefab,
-                transform);
-            powerPostGameObject.transform.localPosition = new Vector3(x + postOffsetX, powerLineAltitude - (powerPostHeight / 2), 0);
-        }        
+                var powerLineGameObject1 = Instantiate(
+                    powerLinePrefab,
+                    InnerTemplate.transform);
+                powerLineGameObject1.transform.localPosition = new Vector3(powerX, powerLineAltitude, -powerLineDistanceZ);
+
+                var powerLineGameObject2 = Instantiate(
+                    powerLinePrefab,
+                    InnerTemplate.transform);
+                powerLineGameObject2.transform.localPosition = new Vector3(powerX, powerLineAltitude, powerLineDistanceZ);
+
+                var powerPostGameObject = Instantiate(
+                    powerPostPrefab,
+                    InnerTemplate.transform);
+                powerPostGameObject.transform.localPosition = new Vector3(x + postOffsetX, powerLineAltitude - (powerPostHeight / 2), 0);
+            }
+        }
+
+        // A clone will suffice since all power lines are the same
+        Inner = Instantiate(InnerTemplate, transform);
     }
 }
