f6389d8d4ceba9574fe240245d5fbbd0cc64f94b
Anders Heed <aheed88888@gmail.com>
2025-07-12 23:11:25 +0200

    add rubiks cube as random alternative welcome plane

--- Assets/3d/WelcomePlane.cs
+++ Assets/3d/WelcomePlane.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WelcomePlane : MonoBehaviour
@@ -6,7 +7,10 @@ public class WelcomePlane : MonoBehaviour
     public float speed = 0.5f;
     public float rollSpeed = 0.5f;
     public float offsetY = 0.2f;
+    public int defaultFlights = 3;
+    public float nonPlaneProbability = 0.1f;
     GameObject inner;
+    int flights = 0;
 
     void Start()
     {
@@ -16,8 +20,26 @@ public class WelcomePlane : MonoBehaviour
 
     void Respawn()
     {
+        flights++;
+        if (flights > defaultFlights)
+        {
+            var innerIndex = 0;
+            if (Random.Range(0f, 1f) < nonPlaneProbability)
+            {
+                // Respawn with a non-plane object
+                innerIndex = Random.Range(1, transform.childCount);
+            }
+            
+            inner = transform.GetChild(innerIndex).gameObject;
+        }
+        
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(child.gameObject == inner);
+        }
+
         var tmpPos = inner.transform.localPosition;
-        tmpPos.y = tmpPos.y <= 0 ? offsetY : -offsetY;
+        tmpPos.y = flights % 2 == 0 ? -offsetY : offsetY;
         tmpPos.x = -maxOffsetX;
         inner.transform.localPosition = tmpPos;
     }
