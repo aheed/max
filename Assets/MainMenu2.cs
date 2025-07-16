ac72a7577f5c949f459628cbc8d57b9cd064699e
Anders Heed <aheed88888@gmail.com>
2025-07-16 16:59:44 +0200

    detect touch in welcome scene

--- Assets/MainMenu2.cs
+++ Assets/MainMenu2.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
@@ -31,7 +32,7 @@ public class MainMenu2 : MonoBehaviour
         new Mission { Title = "Intro", Description = "Let's get you airborne!", LevelType = LevelType.INTRO, SceneName = Mission.ThreeDSceneName },
         new Mission { Title = "2D Classic", Description = "Triple stage mission. 1983 looks better than ever.", LevelType = LevelType.NORMAL, SceneName = Mission.TwoDSceneName },
         new Mission { Title = "2D Balloons", Description = "How many red balloons go by? You guessed it.", LevelType = LevelType.BALLOONS, SceneName = Mission.TwoDSceneName },
-        new Mission { Title = "3D Classic", Description = "Triple stage mission objective again. 3 dimensions. Still 2 wings.", LevelType = LevelType.NORMAL, SceneName = Mission.ThreeDSceneName },
+        new Mission { Title = "3D Classic", Description = "Triple stage mission again. 3 dimensions. Still 2 sets of wings.", LevelType = LevelType.NORMAL, SceneName = Mission.ThreeDSceneName },
         new Mission { Title = "3D Boss Level", Description = "Guest boss appearance. Remember how to defeat this villain?", LevelType = LevelType.ROBOT_BOSS, SceneName = Mission.ThreeDSceneName },
         new Mission { Title = "Red Baron", Description = "How long did you think you could fly a Sopwith Camel without running into this enemy? First person view recommended.", LevelType = LevelType.RED_BARON_BOSS, SceneName = Mission.ThreeDSceneName },
         new Mission { Title = "Dam Busters", Description = "Night mission. Bouncier bombs. Based on a true story. Loosely.", LevelType = LevelType.DAM, SceneName = Mission.ThreeDSceneName },
@@ -71,6 +72,7 @@ public class MainMenu2 : MonoBehaviour
         selectedMissionIndex = Settings.GetSelectedMission();
         Settings.Update();
         GameState.GetInstance().SetPause(false);
+        EnhancedTouchSupport.Enable();
     }
 
     void Update()
@@ -83,6 +85,12 @@ public class MainMenu2 : MonoBehaviour
             {
                 UpdateMissionDetails();
             }
+        }
+
+        if (UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches.Count > 0)
+        {
+            Globals.touchScreenDetected = true;
+            Debug.Log("Touch screen detected");
         }
     }
 
