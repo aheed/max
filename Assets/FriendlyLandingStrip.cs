313b88b04fd90f818dbb1825a27001fdebd2cf36
Anders Heed <aheed88888@gmail.com>
2025-07-27 18:31:03 +0200

    optimize 3D airstrip

--- Assets/FriendlyLandingStrip.cs
+++ Assets/FriendlyLandingStrip.cs
@@ -4,18 +4,22 @@ using UnityEngine.UIElements;
 public class FriendlyLandingStrip : MonoBehaviour
 {
     public AirStripInfo airStripInfo;
+    UIDocument billboardDocument;
+    UIDocument tarmacDocument;
+    static readonly int initialUpdates = 3; // Number of initial updates to ensure UI is updated
+    int updatesToWait = initialUpdates;
 
     void Start()
     {
         airStripInfo ??= AirStripRepository.GetRandomAirStrip();
 
-        var billboardDocument = transform.GetChild(2).GetComponent<UIDocument>();
+        billboardDocument = transform.GetChild(2).GetComponent<UIDocument>();
         SetText(billboardDocument, airStripInfo.name.ToUpper());
         //SetText(billboardDocument, "TOTALLY NOT BEHIND ENEMY LINES AIRSTRIP");
         //SetText(billboardDocument, "CHECKPOINT CHARLIE’S REFUEL & AMMO");
         //SetText(billboardDocument, "REFUEL & RELOAD: ENEMY TERRITORY EDITION");
 
-        var tarmacDocument = transform.GetChild(1).GetComponent<UIDocument>();
+        tarmacDocument = transform.GetChild(1).GetComponent<UIDocument>();
         SetText(tarmacDocument, string.IsNullOrEmpty(airStripInfo.slogan) ? string.Empty : $"\"{airStripInfo.slogan}\"");
     }
 
@@ -23,5 +27,19 @@ public class FriendlyLandingStrip : MonoBehaviour
     {
         var sloganLabel = uiDocument.rootVisualElement.Q<Label>("Slogan");
         sloganLabel.text = slogan;
+    }
+
+    void Update()
+    {
+        if (updatesToWait > 0)
+        {
+            updatesToWait--;
+            if (updatesToWait <= 0)
+            {
+                // Stop updating text for performance reasons
+                billboardDocument.enabled = false;
+                tarmacDocument.enabled = false;
+            }
+        }
     }
 }
